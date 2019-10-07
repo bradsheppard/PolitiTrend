e8a8c74be0dafbb1bcd43ead788693858b3a2108
Brad Sheppard <bradsheppard22@gmail.com>
2019-10-06 23:34:34 -0230

    Added migrations.

--- services/legislator/src/index.ts
+++ services/legislator/src/index.ts
@@ -1,15 +1,7 @@
 import { container } from './inversify.config';
 import App from './App';
 import { TYPES } from './types';
-import SeedExecuter from './seeder/SeedExecuter';
 
-const seedExecuter: SeedExecuter = container.get<SeedExecuter>(TYPES.SeedExecuter);
 const app: App = container.get<App>(TYPES.App);
 
-(async () => {
-    console.log('Seeding database...');
-    await seedExecuter.execute();
-    console.log('Done seeding');
-
-    app.start();
-})();
+app.start();
