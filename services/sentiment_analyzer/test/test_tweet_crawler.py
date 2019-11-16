8c938f50877a019f8590806f3a2d9614795c0a55
Brad Sheppard <bradsheppard22@gmail.com>
2019-11-15 23:48:02 -0330

    updated config

--- services/sentiment_analyzer/test/test_tweet_crawler.py
+++ services/sentiment_analyzer/test/test_tweet_crawler.py
@@ -1,11 +1,18 @@
 import pytest
 from sentiment_analyzer.twitter import TweetCrawler
+import configparser
 
 
 @pytest.fixture
 def tweet_crawler():
-    tweet_crawler = TweetCrawler()
+    config = configparser.ConfigParser()
+    config.read('../config.ini')
+    twitter_config = config['twitter']
+    tweet_crawler = TweetCrawler(twitter_config['consumer_key'], twitter_config['consumer_secret'],
+                                 twitter_config['access_token'], twitter_config['access_token_secret'])
+    return tweet_crawler
 
 
-def test_get():
-    crawler = TweetCrawler()
+def test_get(tweet_crawler):
+    tweets = tweet_crawler.get('Donald Trump')
+    assert len(tweets) > 0
