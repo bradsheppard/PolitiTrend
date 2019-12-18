216aa380726fbe227c160b842e5dd32ae2b7d562
Brad Sheppard <bradsheppard22@gmail.com>
2019-12-17 21:55:01 -0330

    Fixed job handler.

--- services/opinion/src/job_handler/OpinionSummaryJobHandler.ts
+++ services/opinion/src/job_handler/OpinionSummaryJobHandler.ts
@@ -1,32 +1,40 @@
 import JobHandler from './JobHandler';
-import OpinionSummaryJob from '../entity/OpinionSummaryJob';
+import OpinionSummaryJob, { JobStatus } from '../entity/OpinionSummaryJob';
 import { inject, injectable } from 'inversify';
 import { TYPES } from '../types';
 import OpinionRepository from '../entity/repositories/OpinionRepository';
 import OpinionSummaryRepository from '../entity/repositories/OpinionSummaryRepository';
 import OpinionSummary from '../entity/OpinionSummary';
+import OpinionSummaryJobRepository from '../entity/repositories/OpinionSummaryJobRepository';
 
 @injectable()
 class OpinionSummaryJobHandler implements JobHandler<OpinionSummaryJob> {
 
     private readonly opinionRepository: OpinionRepository;
     private readonly opinionSummaryRepository: OpinionSummaryRepository;
+    private readonly opinionSummaryJobRepository: OpinionSummaryJobRepository;
 
     constructor(
         @inject(TYPES.OpinionRepository) opinionRepostory,
-        @inject(TYPES.OpinionSummaryRepository) opinionSummaryRepository)
+        @inject(TYPES.OpinionSummaryRepository) opinionSummaryRepository,
+        @inject(TYPES.OpinionSummaryJobRepository) opinionSummaryJobRepository)
     {
         this.opinionRepository = opinionRepostory;
         this.opinionSummaryRepository = opinionSummaryRepository;
+        this.opinionSummaryJobRepository = opinionSummaryJobRepository;
     }
 
     async handle(job: OpinionSummaryJob): Promise<void> {
         const sentiment = await this.opinionRepository.getSentimentAverageForPolitician(job.politician);
+
         const opinionSummary: OpinionSummary = <OpinionSummary> {
             sentiment,
             politician: job.politician
         };
+
         await this.opinionSummaryRepository.insert(opinionSummary);
+        job.status = JobStatus.Completed;
+        await this.opinionSummaryJobRepository.update(job);
     }
 }
 
