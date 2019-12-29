0f00ff210a5b0429ca37b368781900fbb7001202
Brad Sheppard <bradsheppard22@gmail.com>
2019-12-28 23:01:25 -0330

    Added integration tests.

--- services/politician/src/politicians/politicians.service.spec.ts
+++ services/politician/src/politicians/politicians.service.spec.ts
@@ -1,20 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PoliticiansService } from './politicians.service';
-import { getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import Politician from './politicians.entity';
-import { Repository } from 'typeorm';
 
 describe('PoliticiansService', () => {
 	let service: PoliticiansService;
+	let id = 1;
 
-	beforeEach(async () => {
+	function createPolitician(): Politician {
+		return {
+			party: 'republican',
+			sentiment: id,
+			id,
+			name: `Test ${id}`
+		}
+	}
+
+	beforeAll(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [PoliticiansService,
-				{
-					provide: getRepositoryToken(Politician),
-					useClass: Repository,
-				},
-			],
+			providers: [PoliticiansService],
+			imports: [
+				TypeOrmModule.forRoot(),
+				TypeOrmModule.forFeature([Politician])
+			]
 		}).compile();
 
 		service = module.get<PoliticiansService>(PoliticiansService);
@@ -22,5 +30,21 @@ describe('PoliticiansService', () => {
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+	});
+
+	it('Can get politicians', async () => {
+		const politician = createPolitician();
+		await service.insert(politician);
+		const politicians = await service.get();
+
+		expect(politicians.length).toBeGreaterThan(0);
+	});
+
+	it('Can get specific politician', async () => {
+		const politician = createPolitician();
+		await service.insert(politician);
+		const retrievedPolitician = await service.getOne(politician.id);
+
+		expect(retrievedPolitician).toEqual(politician);
 	});
 });
