3e237bd665c2a94ee52fbb580c4e93c20fdee4f6
Brad Sheppard <bradsheppard22@gmail.com>
2020-01-17 22:25:09 -0330

    Crawler updates.

--- services/crawler/crawler/ml/sentiment_analyzer.py
+++ services/crawler/crawler/ml/sentiment_analyzer.py
@@ -10,8 +10,8 @@ class AnalysisResult:
     subjectResults: Dict[str, float]
 
 
-@dataclass()
-class SubjectResult:
+@dataclass
+class SentenceSubjectResult:
     sentiment: float
     subject: str
     pos: str
@@ -30,22 +30,33 @@ class SentimentAnalyzer:
         analysis_result.sentiment = self._normalize_score(scores['compound'])
         analysis_result.subjectResults = {}
 
-        subject_results = {}
+        sentences_subject_results: Dict[int, Dict[str, SentenceSubjectResult]] = {}
         doc = self._nlp(statement)
         for token in doc:
             subject = self._lookup_subject(token.text)
             if subject is None:
                 continue
-            scores = self._analyzer.polarity_scores(token.sent.text)
-            score = scores['compound']
+            score = self._analyzer.polarity_scores(token.sent.text)['compound']
             adjusted_score = self._normalize_score(score)
-            subject_result = SubjectResult(sentiment=adjusted_score, subject=subject, pos=token.dep_)
-            subject_results[token.sent.start].append(subject_result)
+            subject_result = SentenceSubjectResult(sentiment=adjusted_score, subject=subject, pos=token.dep_)
+            if token.sent.start not in sentences_subject_results:
+                sentences_subject_results[token.sent.start] = {}
+            sentences_subject_results[token.sent.start][subject] = subject_result
 
-            analysis_result.subjectResults[subject] = adjusted_score
-
-        for key in subject_results.keys():
-            subject_results =
+        for sentence_subject_results in sentences_subject_results.values():
+            if len(sentence_subject_results.keys()) == 0:
+                continue
+            elif len(sentence_subject_results.keys()) == 1:
+                key = list(sentence_subject_results.keys())[0]
+                analysis_result.subjectResults[sentence_subject_results[key].subject] = \
+                    sentence_subject_results[key].sentiment
+                analysis_result.sentiment = sentence_subject_results[key].sentiment
+                continue
+            for subject in sentence_subject_results.keys():
+                subject_result = sentence_subject_results[subject]
+                if subject_result.pos == 'nsubj' or subject_result.pos == 'compound':
+                    continue
+                analysis_result.subjectResults[subject_result.subject] = subject_result.sentiment
 
         return analysis_result
 
