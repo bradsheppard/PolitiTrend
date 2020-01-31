0e52c307f66804f20e1e963ec4300781d4b50f97
Brad Sheppard <bradsheppard22@gmail.com>
2020-01-31 20:21:11 -0330

    Added healtcheck endpoint to opinion service.

--- services/opinion/src/app.module.ts
+++ services/opinion/src/app.module.ts
@@ -4,8 +4,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OpinionSummaryModule } from './opinion-summary/opinion-summary.module';
 import { OpinionSummaryJobModule } from './opinion-summary-job/opinion-summary-job.module';
 import { SentimentModule } from './sentiment/sentiment.module';
+import { TerminusModule } from '@nestjs/terminus';
+import { TerminusOptionsService } from './terminus-options/terminus-options.service';
 
 @Module({
-	imports: [TypeOrmModule.forRoot(), OpinionSummaryModule, OpinionSummaryJobModule, SentimentModule, TweetModule],
+	imports: [
+		TypeOrmModule.forRoot(),
+		OpinionSummaryModule,
+		OpinionSummaryJobModule,
+		SentimentModule,
+		TweetModule,
+		TerminusModule.forRootAsync({
+			useClass: TerminusOptionsService,
+		}),
+	],
 })
 export class AppModule {}
