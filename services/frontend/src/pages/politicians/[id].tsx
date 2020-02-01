f11a8f3f643b92ca82e98ee59cd437707962c81a
Brad Sheppard <bradsheppard22@gmail.com>
2020-02-01 20:25:21 -0330

    Frontend React component cleanup.

--- services/frontend/src/pages/politicians/[id].tsx
+++ services/frontend/src/pages/politicians/[id].tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Typography } from '@material-ui/core';
-import PoliticianDetails from '../../components/PoliticianDetails';
+import { Card, createStyles, Divider, Grid, Theme, Typography, withStyles, WithStyles } from '@material-ui/core';
 import { NextPageContext } from 'next';
+import { Tweet as TweetWidget } from 'react-twitter-widgets'
 import ContentContainer from '../../components/ContentContainer';
 import PoliticianApi from '../../apis/politician/PoliticianApi';
 import Bar from '../../components/Bar';
@@ -10,6 +10,21 @@ import TweetApi from '../../apis/tweet/TweetApi';
 import TweetDto from '../../apis/tweet/TweetDto';
 import OpinionSummaryDto from '../../apis/opinion-summary/OpinionSummaryDto';
 import OpinionSummaryApi from '../../apis/opinion-summary/OpinionSummaryApi';
+import PoliticianAvatar from '../../components/PoliticianAvatar';
+import WordCloud from '../../components/WordCloud';
+import LineChart from '../../components/LineChart';
+
+const styles = (theme: Theme) => createStyles({
+    container: {
+        marginTop: theme.spacing(4),
+        marginBottom: theme.spacing(4),
+        textAlign: 'center'
+    },
+    divider: {
+        marginTop: theme.spacing(6),
+        marginBottom: theme.spacing(6)
+    }
+});
 
 interface Tweet {
     tweetId: string;
@@ -29,7 +44,7 @@ interface Politician {
     sentimentHistory: OpinionSummary[];
 }
 
-interface IProps {
+interface IProps extends WithStyles<typeof styles> {
     politician: Politician | null;
 }
 
@@ -39,17 +54,90 @@ const PoliticianPage = (props: IProps) => {
             <Typography>Not Found</Typography>
         );
 
+    const { politician, classes } = props;
+    const tweetTexts = politician.tweets.map(tweet => tweet.tweetText);
+
+    const { tweets } = politician;
+    const firstHalfTweets = politician.tweets.slice(0, tweets.length / 2);
+    const secondHalfTweets = politician.tweets.slice(tweets.length / 2, tweets.length);
+
+    const lineChartData = politician.sentimentHistory.map((opinionSummary: OpinionSummary) => {
+        return {date: new Date(opinionSummary.dateTime), value: opinionSummary.sentiment}
+    });
+
     return (
         <React.Fragment>
             <Bar/>
             <ContentContainer>
-                <PoliticianDetails politician={props.politician} />
+                <Card>
+                    <Grid container
+                          className={classes.container}
+                          alignItems='center'
+                          direction='row'
+                          justify='center'>
+                        <Grid item
+                              sm={6}>
+                            <PoliticianAvatar politician={politician} />
+                        </Grid>
+                        <Grid item
+                              sm={6}>
+                            <WordCloud statements={tweetTexts} exclusion={politician.name} />
+                        </Grid>
+                        { lineChartData.length > 0 ?
+                            (
+                                <Grid item
+                                      sm={12}>
+                                    <LineChart data={lineChartData} xAxis='Time' yAxis='Popularity'/>
+                                </Grid>
+                            ) : null
+                        }
+                        <Grid item
+                              sm={12}>
+                            <Divider variant="middle" className={classes.divider} />
+                            <Grid container
+                                  alignItems='flex-start'
+                                  direction='row'
+                                  justify='center'>
+                                <Grid item sm={6}>
+                                    {
+                                        firstHalfTweets.map((opinion: Tweet, index) => {
+                                            return (
+                                                <TweetWidget
+                                                    options={{
+                                                        align: 'center'
+                                                    }}
+                                                    tweetId={opinion.tweetId}
+                                                    key={index}
+                                                />
+                                            )
+                                        })
+                                    }
+                                </Grid>
+                                <Grid item sm={6}>
+                                    {
+                                        secondHalfTweets.map((opinion: Tweet, index) => {
+                                            return (
+                                                <TweetWidget
+                                                    options={{
+                                                        align: 'center'
+                                                    }}
+                                                    tweetId={opinion.tweetId}
+                                                    key={index}
+                                                />
+                                            )
+                                        })
+                                    }
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Card>
             </ContentContainer>
         </React.Fragment>
     )
 };
 
-PoliticianPage.getInitialProps = async function(context: NextPageContext): Promise<IProps> {
+PoliticianPage.getInitialProps = async function(context: NextPageContext) {
     const { id } = context.query;
     if (typeof id === 'string') {
         const politicianDto: PoliticianDto | null = await PoliticianApi.getOne(context, parseInt(id));
@@ -81,4 +169,4 @@ PoliticianPage.getInitialProps = async function(context: NextPageContext): Promi
     }
 };
 
-export default PoliticianPage;
+export default withStyles(styles)(PoliticianPage);
