7acefb8e64b89a781cc6302b2f519b610bc068b5
Brad Sheppard <bradsheppard22@gmail.com>
2020-03-28 21:30:44 -0230

    - Added spark application analytic for word count. - Increased number of nodes in K8s cluster. - Mods to word-cloud-analytic project and build.

--- services/word-cloud-analytic/build.sbt
+++ services/word-cloud-analytic/build.sbt
@@ -4,8 +4,17 @@ version := "1.0"
 
 scalaVersion := "2.11.11"
 
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.5"
-libraryDependencies += "org.apache.hadoop" % "hadoop-aws" % "2.8.2"
-
-libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.4.5"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.5" % "provided"
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.4.5" % "provided"
 libraryDependencies += "org.apache.spark" % "spark-sql-kafka-0-10_2.11" % "2.4.5"
+
+libraryDependencies += "com.amazonaws" % "aws-java-sdk" % "1.7.4"
+libraryDependencies += "org.apache.hadoop" % "hadoop-common" % "2.7.3"
+
+unmanagedBase := baseDirectory.value / "lib"
+
+assemblyMergeStrategy in assembly := {
+  case "META-INF/services/org.apache.spark.sql.sources.DataSourceRegister" => MergeStrategy.concat
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
--- services/word-cloud-analytic/src/main/scala/TweetWordCount.scala
+++ services/word-cloud-analytic/src/main/scala/TweetWordCount.scala
@@ -6,77 +6,79 @@ import org.apache.spark.sql.functions._
 
 object TweetWordCount {
 
-  case class Tweet(tweetText: String, tweetId: String, sentiments: Seq[Sentiment])
-  case class Sentiment(politician: Long, value: Double)
+    case class Tweet(tweetText: String, tweetId: String, sentiments: Seq[Sentiment])
 
-  case class WordCount(politician: Long, word: String, count: Long)
+    case class Sentiment(politician: Long, value: Double)
 
-  case class CreateWord(word: String, count: Long)
-  case class CreateWordCloud(politician: Long, words: Seq[CreateWord])
+    case class WordCount(politician: Long, word: String, count: Long)
 
-  def main(args: Array[String]) {
-    val spark = SparkSession.builder
-      .master("local[*]")
-      .appName("Tweet Word Count")
-      .getOrCreate()
+    case class CreateWord(word: String, count: Long)
 
-    import spark.implicits._
+    case class CreateWordCloud(politician: Long, words: Seq[CreateWord])
 
-    val sc = spark.sparkContext
+    def main(args: Array[String]) {
+        val spark = SparkSession.builder
+            .appName("Tweet Word Count")
+            .getOrCreate()
 
-    val now = Calendar.getInstance()
-    val currentYear = now.get(Calendar.YEAR)
-    val currentMonth = "%02d".format(now.get(Calendar.MONTH) + 1)
-    val currentDay = now.get(Calendar.DAY_OF_MONTH)
+        import spark.implicits._
 
-    val s3Path = "s3a://tweets/topics/tweet-created/" +
-      s"year=${currentYear}/month=${currentMonth}/day=${currentDay}/hour=01"
+        val sc = spark.sparkContext
 
-    sc.hadoopConfiguration.set("fs.s3a.access.key", "brad1234")
-    sc.hadoopConfiguration.set("fs.s3a.secret.key", "brad1234")
-    sc.hadoopConfiguration.set("fs.s3a.path.style.access", "true")
-    sc.hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    sc.hadoopConfiguration.set("fs.s3a.endpoint", "http://minio:9000")
+        val now = Calendar.getInstance()
+        val currentYear = now.get(Calendar.YEAR)
+        val currentMonth = "%02d".format(now.get(Calendar.MONTH) + 1)
+        val currentDay = now.get(Calendar.DAY_OF_MONTH)
 
-    val dataframe: Dataset[Tweet] = spark.read.json(s3Path).as[Tweet].persist()
+        val s3Path = "s3a://tweets/topics/tweet-created/" +
+            s"year=${currentYear}/month=${currentMonth}/day=${currentDay}/hour=01"
 
-    val makeWord = udf((word: String, count: Long) => CreateWord(word, count))
+        sc.hadoopConfiguration.set("fs.s3a.access.key", "brad1234")
+        sc.hadoopConfiguration.set("fs.s3a.secret.key", "brad1234")
+        sc.hadoopConfiguration.set("fs.s3a.path.style.access", "true")
+        sc.hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+        sc.hadoopConfiguration.set("fs.s3a.endpoint", "http://minio:9000")
+        sc.hadoopConfiguration.set("fs.s3a.connection.ssl.enabled", "false")
 
-    val w = Window.partitionBy($"politician").orderBy($"count".desc)
+        val dataframe: Dataset[Tweet] = spark.read.json(s3Path).as[Tweet].persist()
 
-    var wordCountDataFrame: Dataset[WordCount] = dataframe
-        .withColumn("word",
-          explode(
-            split($"tweetText", "\\s+")
-          )
-        )
-        .withColumn("politician", explode($"sentiments.politician"))
+        val makeWord = udf((word: String, count: Long) => CreateWord(word, count))
 
-        .groupBy("word", "politician")
-        .count().as[WordCount]
+        val w = Window.partitionBy($"politician").orderBy($"count".desc)
 
-    wordCountDataFrame = wordCountDataFrame
-      .filter(x => x.word.length() >= 5)
-      .withColumn("row_number", row_number.over(w))
-      .where($"row_number" <= 10).as[WordCount]
+        var wordCountDataFrame: Dataset[WordCount] = dataframe
+            .withColumn("word",
+                explode(
+                    split($"tweetText", "\\s+")
+                )
+            )
+            .withColumn("politician", explode($"sentiments.politician"))
 
-    val politicianWordCountDataFrame: Dataset[CreateWordCloud] = wordCountDataFrame.groupBy($"politician")
-      .agg(
-        collect_list(
-          makeWord(col("word"), col("count"))
-        ).as("words").as[Set[CreateWord]]
-      ).as[CreateWordCloud]
+            .groupBy("word", "politician")
+            .count().as[WordCount]
 
-    val jsonifiedDataframe = politicianWordCountDataFrame.toJSON
-    jsonifiedDataframe.show()
+        wordCountDataFrame = wordCountDataFrame
+            .filter(x => x.word.length() >= 5)
+            .withColumn("row_number", row_number.over(w))
+            .where($"row_number" <= 10).as[WordCount]
 
-    jsonifiedDataframe.select($"value")
-      .write
-      .format("kafka")
-      .option("kafka.bootstrap.servers", "queue-kafka-bootstrap:9092")
-      .option("topic", "word-cloud-created")
-      .save()
+        val politicianWordCountDataFrame: Dataset[CreateWordCloud] = wordCountDataFrame.groupBy($"politician")
+            .agg(
+                collect_list(
+                    makeWord(col("word"), col("count"))
+                ).as("words").as[Set[CreateWord]]
+            ).as[CreateWordCloud]
 
-    spark.stop()
-  }
+        val jsonifiedDataframe = politicianWordCountDataFrame.toJSON
+        jsonifiedDataframe.show()
+
+        jsonifiedDataframe.select($"value")
+            .write
+            .format("kafka")
+            .option("kafka.bootstrap.servers", "queue-kafka-bootstrap:9092")
+            .option("topic", "word-cloud-created")
+            .save()
+
+        spark.stop()
+    }
 }
