3bcf54005242c05a8f0912be028419a93c350b6c
Brad Sheppard <bradsheppard22@gmail.com>
2020-04-05 18:02:12 -0230

    Frontend refactoring.

---
+++ services/frontend/src/components/politicians/PoliticiansGridList.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import PoliticiansGridListItem from './PoliticiansGridListItem';
+import { createStyles, Grid, Theme, Link as MuiLink } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import Link from 'next/link';
+
+interface IProps {
+    politicians: Politician[];
+}
+
+interface Politician {
+    id: number;
+    name: string;
+    party: string;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            paddingTop: theme.spacing(2),
+            paddingBottom: theme.spacing(2)
+        }
+    })
+);
+
+const PoliticiansGridList = (props: IProps) => {
+    const classes = useStyles();
+
+    return (
+        <React.Fragment>
+            <Grid container>
+            {
+                props.politicians.map((politician: Politician, index: number) => {
+                    return (
+                        <Grid item xs={6}>
+                            <Link href='/politicians/[id]' as={`/politicians/${politician.id}`}>
+                                <MuiLink href='#'>
+                                    <PoliticiansGridListItem politician={politician} key={index} className={classes.container} />
+                                </MuiLink>
+                            </Link>
+                        </Grid>
+                    );
+                })
+            }
+            </Grid>
+        </React.Fragment>
+    );
+};
+
+export default PoliticiansGridList;
---
+++ services/frontend/src/components/politicians/PoliticiansGridListItem.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import { Box, createStyles, Theme, Typography } from '@material-ui/core';
+import { politicianNameToImagePath } from '../../utils/ImagePath';
+import { makeStyles } from '@material-ui/styles';
+import clsx from 'clsx';
+import Fade from '../common/Fade';
+
+interface IProps {
+    politician: Politician;
+}
+
+interface Politician {
+    id: number;
+    name: string;
+    party: string;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        image: {
+            position: 'absolute',
+            margin: 'auto',
+            width: '100%',
+            top: '-80%',
+            bottom: '-100%',
+            left: '-100%',
+            right: '-100%',
+        },
+        imageContainer: {
+            marginRight: theme.spacing(4),
+            position: 'relative',
+            float: 'left',
+            overflow: 'hidden',
+            height: theme.spacing(30),
+            width: theme.spacing(30)
+        },
+        textContainer: {
+            float: 'left'
+        },
+        container: {
+            clear: 'both',
+            overflow: 'auto'
+        }
+    })
+);
+
+const PoliticiansGridListItem = (props: IProps & React.HTMLAttributes<HTMLDivElement>) => {
+    const classes = useStyles();
+
+    return (
+        <Fade>
+            <div className={clsx(classes.container, props.className)}>
+                <div className={classes.imageContainer}>
+                    <img src={politicianNameToImagePath(props.politician.name)} alt={props.politician.name} className={classes.image} />
+                </div>
+                <div className={classes.textContainer}>
+                    <Typography variant='h4' color='textPrimary'>
+                        <Box fontWeight='fontWeightBold'>
+                            {props.politician.name}
+                        </Box>
+                    </Typography>
+                    <Typography variant='h6' color='textSecondary'>
+                        {props.politician.party}
+                    </Typography>
+                    <Typography variant='h6' color='textSecondary'>
+                        Senator
+                    </Typography>
+                </div>
+            </div>
+        </Fade>
+    );
+};
+
+export default PoliticiansGridListItem;
