324801f1d1b62c5982f0965409ea8d66d791ea62
Brad Sheppard <bradsheppard22@gmail.com>
2020-05-16 13:42:29 -0230

    Updated api

--- services/frontend/src/pages/api/wordclouds.ts
+++ services/frontend/src/pages/api/wordclouds.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 import WordCloudDto from '../../apis/word-cloud/WordCloudDto';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse<WordCloudDto[]>) {
-    const axiosResponse = await axios.get<WordCloudDto[]>('http://word-cloud/global-word-cloud', {params: req.query});
+    const axiosResponse = await axios.get<WordCloudDto[]>('http://analytics/global-word-cloud', {params: req.query});
     res.json(axiosResponse.data);
 }
