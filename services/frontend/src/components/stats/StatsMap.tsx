f9b69b6eedca64b8001c0e014bbaef6cc0f841bb
Brad Sheppard <bradsheppard22@gmail.com>
2020-07-20 22:34:45 -0230

    State map changes

--- services/frontend/src/components/stats/StatsMap.tsx
+++ services/frontend/src/components/stats/StatsMap.tsx
@@ -2,8 +2,29 @@ import * as React from 'react';
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
 
 import allStates from './allstates.json';
+import { scaleQuantize } from 'd3-scale';
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
+
+const republicanScale = scaleQuantize<string>()
+    .domain([0, 0.05])
+    .range([
+        "#cc9b98",
+        "#cc7372",
+        "#cc5959",
+        "#cc4542",
+        "#CC2C26"
+    ]);
+
+const democraticScale = scaleQuantize<string>()
+    .domain([0, 0.05])
+    .range([
+        "#99aacd",
+        "#8299cd",
+        "#6483cd",
+        "#4e74cd",
+        "#3463cd"
+    ]);
 
 interface IProps {
     statePartyAffiliations: StatePartyAffiliation[];
@@ -35,9 +56,15 @@ const StatsMap = (props: IProps) => {
                             const statePartyAffiliation = lookupStatePartyAffiliation(state.id);
                             if(!statePartyAffiliation)
                                 return;
-                            const color =
-                                statePartyAffiliation.affiliations.democratic > statePartyAffiliation.affiliations.republican ?
-                                    '#3463cd' : '#CC2C26';
+
+                            let color;
+                            if(statePartyAffiliation.affiliations.democratic > statePartyAffiliation.affiliations.republican)
+                                color = democraticScale(statePartyAffiliation.affiliations.democratic - statePartyAffiliation.affiliations.republican);
+                            else if(statePartyAffiliation.affiliations.democratic < statePartyAffiliation.affiliations.republican)
+                                color = republicanScale(statePartyAffiliation.affiliations.republican - statePartyAffiliation.affiliations.democratic);
+                            else
+                                color = '#33333';
+
                             return (
                                 <Geography
                                     key={geo.rsmKey}
