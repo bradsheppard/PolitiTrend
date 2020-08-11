cf105639b91361d3b6987f7f4eb272ddb79495d6
Brad Sheppard <bradsheppard22@gmail.com>
2020-08-10 22:15:13 -0230

    Changed app bar to not move on scroll

--- services/frontend/src/components/bar/Bar.tsx
+++ services/frontend/src/components/bar/Bar.tsx
@@ -3,7 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Globals from '../../utils/Globals';
-import { makeStyles, Slide, Theme, useScrollTrigger } from '@material-ui/core';
+import { makeStyles, Theme } from '@material-ui/core';
 import BarItem from './BarItem';
 import Link from 'next/link';
 import FacebookIcon from '@material-ui/icons/Facebook';
@@ -33,66 +33,40 @@ const useStyles = makeStyles((theme: Theme) => ({
     }
 }));
 
-interface Props {
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    window?: () => Window;
-    children?: React.ReactElement;
-    overlay?: boolean;
-}
-
-function HideOnScroll(props: Props) {
-    const { children, window } = props;
-    const trigger = useScrollTrigger({ target: window ? window() : undefined });
-
-    return (
-        <Slide appear={false} direction="down" in={!trigger}>
-            {children}
-        </Slide>
-    );
-}
-
-const Header = (props: Props) => {
+const Header = () => {
     const classes = useStyles();
 
     return (
         <React.Fragment>
-            <HideOnScroll {...props}>
-                <AppBar position="fixed" color="secondary" style={{ backgroundColor: 'rgba(0,0,0,0.9)'}}>
-                    <Toolbar>
-                        <div className={classes.title}>
-                            <Link href='/' passHref>
-                                <Typography variant="h6" component='a' className={classes.titleText}>
-                                    {capitalize(Globals.name)}
-                                </Typography>
-                            </Link>
-                        </div>
-                        <div className={classes.items}>
-                            <BarItem link='/'>
-                                Home
-                            </BarItem>
-                            <BarItem link='/politicians'>
-                                Politicians
-                            </BarItem>
-                            <BarItem link='/stats'>
-                                Stats
-                            </BarItem>
-                            <BarItem link='/about'>
-                                About
-                            </BarItem>
-                        </div>
-                        <div className={classes.icons}>
-                            <FacebookIcon className={classes.icon} />
-                            <TwitterIcon className={classes.icon} />
-                        </div>
-                    </Toolbar>
-                </AppBar>
-            </HideOnScroll>
-            {
-                !props.overlay ? <Toolbar/> : null
-            }
+            <AppBar position="fixed" color="secondary" style={{ backgroundColor: 'rgba(0,0,0,0.9)'}}>
+                <Toolbar>
+                    <div className={classes.title}>
+                        <Link href='/' passHref>
+                            <Typography variant="h6" component='a' className={classes.titleText}>
+                                {capitalize(Globals.name)}
+                            </Typography>
+                        </Link>
+                    </div>
+                    <div className={classes.items}>
+                        <BarItem link='/'>
+                            Home
+                        </BarItem>
+                        <BarItem link='/politicians'>
+                            Politicians
+                        </BarItem>
+                        <BarItem link='/stats'>
+                            Stats
+                        </BarItem>
+                        <BarItem link='/about'>
+                            About
+                        </BarItem>
+                    </div>
+                    <div className={classes.icons}>
+                        <FacebookIcon className={classes.icon} />
+                        <TwitterIcon className={classes.icon} />
+                    </div>
+                </Toolbar>
+            </AppBar>
         </React.Fragment>
 
     );
