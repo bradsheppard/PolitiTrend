3f6271cdab7debf7bf2f04b691981adc0340898d
Brad Sheppard <bradsheppard22@gmail.com>
2020-10-26 10:12:31 -0230

    Updated Sentiment analyzer

--- services/state-party-affiliation-analytic/state_party_affiliation_analytic/sentiment_analyzer.py
+++ services/state-party-affiliation-analytic/state_party_affiliation_analytic/sentiment_analyzer.py
@@ -1,5 +1,7 @@
 import spacy
-from typing import Dict, List, Union
+from typing import Dict, List
+import networkx as nx
+from functional import seq
 from statistics import mean
 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
@@ -7,82 +9,82 @@ from state_party_affiliation_analytic.politician import Politician
 
 sentiment_analyzer = SentimentIntensityAnalyzer()
 
-nlp = spacy.load('en', disable=['ner', 'entity_linker', 'textcat', 'entity_ruler',
-                                'merge_entities'])
+nlp = spacy.load('en')
 
 
 def get_party_sentiments(statements: List[str],
                          subjects: List[Politician] = None) -> List[Dict[str, float]]:
+    # pylint: disable=cell-var-from-loop
+    # pylint: disable=too-many-locals
     results_list = []
     for doc in nlp.pipe(statements):
         results = {}
         for sent in doc.sents:
             score = sentiment_analyzer.polarity_scores(sent.text)['compound']
-            pos_subjects = _get_pos_subjects(sent, ['VERB', 'ADJ', 'NOUN'], subjects)
+            pos_words = _get_pos_subjects(sent, ['VERB', 'ADJ', 'NOUN'])
+            entities = seq(sent.ents) \
+                .filter(lambda x: x.label_ == 'PERSON') \
+                .map(lambda x: x.text) \
+                .list()
 
-            max_length = 0
-            for subject in pos_subjects.values():
-                if len(subject) > max_length:
-                    max_length = len(subject)
+            if len(entities) == 0:
+                results_list.append(results)
+                continue
 
-            for key in pos_subjects:
-                if len(pos_subjects[key]) == max_length and max_length != 0:
-                    if key not in results:
-                        results[key] = [score]
+            graph = nx.Graph()
+
+            for token in sent:
+                for child in token.children:
+                    dep = child.dep_
+                    weight = 0 if dep == 'conj' else 1
+                    graph.add_edge('{0}'.format(token), '{0}'.format(child), weight=weight)
+
+            path_lengths = {entity: 0 for entity in entities}
+
+            for word in pos_words:
+                for entity in entities:
+                    names = entity.split()
+                    shortest_path = min(
+                        [nx.dijkstra_path_length(
+                            graph, source=word, target=name) for name in names])
+                    path_lengths[entity] += shortest_path
+
+            shortest_length = min(path_lengths.values())
+
+            relevant_entities = seq(path_lengths.items()) \
+                .filter(lambda x: x[1] == shortest_length) \
+                .map(lambda x: x[0])
+
+            for entity in relevant_entities:
+                politicians = _match_politicians(entity, subjects)
+                for politician in politicians:
+                    if politician.party not in results:
+                        results[politician.party] = [score]
                     else:
-                        results[key].append(score)
+                        results[politician.party].append(score)
 
         for key in results:
             results[key] = mean(results[key])
-
         results_list.append(results)
 
     return results_list
 
 
-def _get_pos_subjects(doc, pos_list, politicians) -> Dict[str, List[str]]:
-    verbs = {politician.party: [] for politician in politicians}
-    for possible_verb in doc:
-        if possible_verb.pos_ in pos_list:
-            found_child = False
-            children = possible_verb.children
-            for child in children:
-                match = _match_politician(child.text, politicians)
-                if match is not None and child.dep_ == 'nsubj':
-                    verbs[match.party].append(possible_verb)
-                    _traverse_subject_conjs(child, possible_verb, verbs, politicians)
-                    found_child = True
-            if not found_child:
-                _traverse_up(possible_verb, possible_verb, verbs, politicians)
-    return verbs
+def _get_pos_subjects(doc, pos_list) -> List[str]:
+    results = []
+    for word in doc:
+        if word.pos_ in pos_list:
+            results.append(word.text)
+
+    return results
 
 
-def _match_politician(text, politicians) -> Union[Politician, None]:
+def _match_politicians(text, politicians) -> List[Politician]:
+    results = []
+
     for politician in politicians:
         split_name = politician.name.split()
-        if text in split_name:
-            return politician
-    return None
-
-
-def _traverse_up(possible_verb, current, verbs, politicians):
-    head = current.head
-    if current == head:
-        return
-    children = head.children
-    for child in children:
-        match = _match_politician(child.text, politicians)
-        if match is not None and child.dep_ == 'nsubj':
-            verbs[match.party].append(possible_verb)
-            _traverse_subject_conjs(child, possible_verb, verbs, politicians)
-    _traverse_up(possible_verb, head, verbs, politicians)
-
-
-def _traverse_subject_conjs(subj, possible_verb, verbs, politicians):
-    children = subj.children
-    for child in children:
-        if child.dep_ == 'conj':
-            match = _match_politician(child.text, politicians)
-            if match is not None:
-                verbs[match.party].append(possible_verb)
-                _traverse_subject_conjs(child, possible_verb, verbs, politicians)
+        for name in split_name:
+            if name in text:
+                results.append(politician)
+    return results
