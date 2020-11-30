43200b71715102bb2128fcd0fb1b4776f42005e1
Brad Sheppard <bradsheppard22@gmail.com>
2020-11-30 16:17:27 -0330

    Added legend to sentiment table

--- services/frontend/src/components/stats/StatsSentimentTable.tsx
+++ services/frontend/src/components/stats/StatsSentimentTable.tsx
@@ -279,6 +279,32 @@ const StatsSentimentTable: React.FC<IProps> = (
                             useUTC: false,
                             precision: 'day',
                         }}
+                        legends={[
+                            {
+                                anchor: 'bottom-right',
+                                direction: 'column',
+                                justify: false,
+                                translateX: 100,
+                                translateY: 0,
+                                itemsSpacing: 0,
+                                itemDirection: 'left-to-right',
+                                itemWidth: 80,
+                                itemHeight: 20,
+                                itemOpacity: 0.75,
+                                symbolSize: 12,
+                                symbolShape: 'circle',
+                                symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                                effects: [
+                                    {
+                                        on: 'hover',
+                                        style: {
+                                            itemBackground: 'rgba(0, 0, 0, .03)',
+                                            itemOpacity: 1,
+                                        },
+                                    },
+                                ],
+                            },
+                        ]}
                         xFormat="time:%Y-%m-%d"
                         yScale={{ type: 'linear', min: 1, max: 10, stacked: false, reverse: false }}
                         axisTop={null}
