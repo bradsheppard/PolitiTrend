2c4ee55924fc23f85a79bec8c996755f3edb226d
Brad Sheppard <bradsheppard22@gmail.com>
2021-03-08 15:36:15 -0330

    Moved config for word cloud analytics into file.

--- services/word-cloud-analytic/src/main/scala/com/voyce/common/ConfigReader.scala
+++ services/word-cloud-analytic/src/main/scala/com/voyce/common/ConfigReader.scala
@@ -1,19 +1,12 @@
 package com.voyce.common
 
-import java.io.{File, InputStream}
+import java.io.{FileInputStream, InputStream}
 import java.util.Properties
 
 import org.apache.spark.SparkContext
 
-import scala.io.{BufferedSource, Source}
-
 class ConfigReader {
-
-    private val url: InputStream = getClass.getResourceAsStream("/application.properties")
-    private val properties: Properties = new Properties ()
-
-    private val source: BufferedSource = Source.fromInputStream(url)
-    properties.load(source.bufferedReader())
+    private val properties = new Properties()
 
     private final val ACCESS_KEY = "fs.s3a.access.key"
     private final val SECRET_KEY = "fs.s3a.secret.key"
@@ -29,6 +22,11 @@ class ConfigReader {
     }
 
     def load(sc: SparkContext): Unit = {
+        val inputStream = new FileInputStream("/app/application.properties")
+        properties.load(inputStream)
+
+        inputStream.close()
+
         sc.hadoopConfiguration.set(ACCESS_KEY, properties.getProperty(ACCESS_KEY))
         sc.hadoopConfiguration.set(SECRET_KEY, properties.getProperty(SECRET_KEY))
         sc.hadoopConfiguration.set(PATH_STYLE_ACCESS, properties.getProperty(PATH_STYLE_ACCESS))
