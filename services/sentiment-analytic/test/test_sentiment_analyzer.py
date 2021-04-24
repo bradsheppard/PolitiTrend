8e90f964b27194d897d065ca9c334e5db75eca22
Brad Sheppard <bradsheppard22@gmail.com>
2021-04-24 11:56:19 -0230

    Linting fixes

--- services/sentiment-analytic/test/test_sentiment_analyzer.py
+++ services/sentiment-analytic/test/test_sentiment_analyzer.py
@@ -1,8 +1,9 @@
+# pylint: disable=redefined-outer-name
+
 from typing import List
 
 import pytest
 
-from numba import cuda
 from sentiment_analytic.politician import Politician
 from sentiment_analytic.sentiment_analyzer import SentimentAnalyzer
 
@@ -19,26 +20,26 @@ def politicians():
 
 @pytest.fixture(scope='module')
 def sentiment_analyzer():
-    yield SentimentAnalyzer()
-
-    device = cuda.get_current_device()
-    device.reset()
+    return SentimentAnalyzer()
 
 
-def test_get_entity_sentiments_postive_sentence(politicians: List[Politician], sentiment_analyzer: SentimentAnalyzer):
+def test_get_entity_sentiments_postive_sentence(politicians: List[Politician],
+                                                sentiment_analyzer: SentimentAnalyzer):
     sentence = 'John Smith is great!'
 
     predictions = sentiment_analyzer.get_entity_sentiments([sentence], [politicians])
     assert predictions[0][2] > 0
 
 
-def test_get_entity_sentiments_negative_sentence(politicians: List[Politician], sentiment_analyzer: SentimentAnalyzer):
+def test_get_entity_sentiments_negative_sentence(politicians: List[Politician],
+                                                 sentiment_analyzer: SentimentAnalyzer):
     sentence = 'John Smith is terrible'
     predictions = sentiment_analyzer.get_entity_sentiments([sentence], [politicians])
     assert predictions[0][2] < 0
 
 
-def test_get_entity_sentiments_subject_results(politicians: List[Politician], sentiment_analyzer: SentimentAnalyzer):
+def test_get_entity_sentiments_subject_results(politicians: List[Politician],
+                                               sentiment_analyzer: SentimentAnalyzer):
     sentence = 'Bob Young is awesome. John Smith is terrible though.'
     predictions = sentiment_analyzer.get_entity_sentiments([sentence], [politicians])
     bob_score = predictions[0][1]
@@ -47,7 +48,8 @@ def test_get_entity_sentiments_subject_results(politicians: List[Politician], se
     assert john_score < 0
 
 
-def test_get_entity_sentiments_non_specific_subject(politicians: List[Politician], sentiment_analyzer: SentimentAnalyzer):
+def test_get_entity_sentiments_non_specific_subject(politicians: List[Politician],
+                                                    sentiment_analyzer: SentimentAnalyzer):
     sentence = 'I\'m awesome'
     predictions = sentiment_analyzer.get_entity_sentiments([sentence], [politicians])
     assert 1 not in predictions[0]
